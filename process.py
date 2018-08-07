fb9fa56e10fb1f02dab5fb06bf1d01ab8f56c15d
Magdalena Stenius <magdalena.stenius@bimobject.com>
2018-08-07 15:43:46 +0200

    Not sure what I did here

--- process.py
+++ process.py
@@ -1,7 +1,21 @@
 from PIL import Image
 import numpy as np
 import pandas as pd
-from os import listdir, walk
+from os import listdir, walk, remove
+
+def convert_to_bw(path, name):
+    # for example convert_to_bw("Images\\", "ONE.JPG")
+    image_file = Image.open(path+name) # Open colour image
+    image_file = image_file.convert("L") # Convert the image to b&w
+    image_file.save(path+"BW_"+name)
+    remove(path+name)
+    return path+"BW_"+name
+
+def convert_to_png(path, name):
+    image_file = Image.open(path+name)
+    image_file.save(path + name[:-4]+".png")
+    remove(path+name)
+    return name[:-4]+".png"
 
 def process_image(image_file, count):
     """Process Image
@@ -20,32 +34,34 @@ def process_image(image_file, count):
     WIDTH = 100
     HEIGHT = 100
     EXT = ".jpg"
+
+    #image_path = INPUT_PATH + image_file
     
+    # Step 0: Convert to png and to b&w
+    png_name = convert_to_png(INPUT_PATH, image_file)
+    image_path = convert_to_bw(INPUT_PATH, png_name)
+
     # Step 1: image_file = "pic.jpg" Open with Pillow
-    image_path = INPUT_PATH + image_file
     im1 = Image.open(image_path)
 
     # If not width == height, resize (check params..)
     width_1, height_1 = im1.size
     if width_1 > height_1:
         diff = width_1-height_1
-        im1.crop((0, diff/2, width_1, diff/2))
+        im1.crop((diff/2, 0, diff/2, height_1))
     elif width_1 < height_1:
         diff = height_1-width_1
-        im1.crop((0, 0, diff/2, diff/2))
+        im1.crop((0, diff/2, width_1, diff/2))
     
     # Step 2: Use antialias filter to apply resize
     im2 = im1.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
-
-    # Step 3: Convert to b&w
-    im3 = im2.convert('LA')
 
     # Save
     im2.save(OUTPUT_PATH+count+EXT)
 
     # Return numpy array
-    img_np = np.array(list(im3.getdata(band=0)), float)
-    img_np.shape = (im3.size[1], im3.size[0])
+    img_np = np.array(list(im2.getdata(band=0)), float)
+    img_np.shape = (im2.size[1], im2.size[0])
     return img_np
 
 def process_all():
